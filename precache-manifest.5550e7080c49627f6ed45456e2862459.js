2bd921ce95b96e68242fa7c703a2fad8a7c8aabf
David Tenenbaum <github email address>
2018-10-25 10:00:37 -0400

    Updates

--- precache-manifest.5550e7080c49627f6ed45456e2862459.js
+++ precache-manifest.5550e7080c49627f6ed45456e2862459.js
@@ -8,15 +8,15 @@ self.__precacheManifest = [
     "url": "/Portfolio/static/js/main.5093f08f.chunk.js"
   },
   {
-    "revision": "f77b91ec8d2ca51c04ad",
-    "url": "/Portfolio/static/js/1.f77b91ec.chunk.js"
+    "revision": "1f870015660c9540f391",
+    "url": "/Portfolio/static/js/1.1f870015.chunk.js"
   },
   {
     "revision": "5093f08f7e37497bcd47",
     "url": "/Portfolio/static/css/main.2eb6f22d.chunk.css"
   },
   {
-    "revision": "eb12018d5d754b67942e38d38b7feb3d",
+    "revision": "bd1a3428e50e640fa09acc680e900f93",
     "url": "/Portfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -11,10 +11,10 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.fe57e804f1f0f3749c7cde187d11f803.js"
+  "/Portfolio/precache-manifest.5550e7080c49627f6ed45456e2862459.js"
 );
 
 workbox.clientsClaim();
