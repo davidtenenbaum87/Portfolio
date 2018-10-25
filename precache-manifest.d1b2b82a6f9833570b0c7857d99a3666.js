ed74807ffdc612905ea82912df329cf57150a230
David Tenenbaum <github email address>
2018-10-25 08:55:45 -0400

    Updates

---
+++ precache-manifest.d1b2b82a6f9833570b0c7857d99a3666.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b96116072eb85f5a3a2",
+    "url": "/Portfolio/static/js/runtime~main.3b961160.js"
+  },
+  {
+    "revision": "a4e567a9d333abe0d6de",
+    "url": "/Portfolio/static/js/main.a4e567a9.chunk.js"
+  },
+  {
+    "revision": "f77b91ec8d2ca51c04ad",
+    "url": "/Portfolio/static/js/1.f77b91ec.chunk.js"
+  },
+  {
+    "revision": "a4e567a9d333abe0d6de",
+    "url": "/Portfolio/static/css/main.7df51767.chunk.css"
+  },
+  {
+    "revision": "f16ee4d7ed6d6823c199ac51bc6e2d91",
+    "url": "/Portfolio/index.html"
+  }
+];
